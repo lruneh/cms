3a9a7d1d6a1da7afdf874415034be0bf9d8d3b25
Lasse Rune Hansen <lrh@akait.dk>
2022-01-12 15:38:39 +0100

    Add project files.

---
+++ cms/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
